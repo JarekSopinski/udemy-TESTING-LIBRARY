e9e0b2d3efd5f00ebd872a786784612814f14df3
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-11 19:54:22 +0100

    React code for Options and ScoopOption components

--- sundae-starter/src/pages/entry/Options.jsx
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -1,5 +1,31 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Row } from 'react-bootstrap';
+import ScoopOption from './ScoopOption';
+
+// eslint-disable-next-line react/prop-types
 const Options = ({ optionType }) => {
-    return <div>Options</div>;
+    // optionType is 'scoops' or 'toppings'
+
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://localhost:3030/${optionType}`)
+            .then(res => setItems(res))
+            .catch(err => console.error(err))
+    }, [optionType]);
+
+    const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+    const optionItems = items.map(item => (
+        <ItemComponent
+            key={item.name}
+            name={item.name}
+            imagePath={item.imagePath}
+        />
+    ));
+
+    return <Row>{optionItems}</Row>;
 };
 
 export default Options;
--- sundae-starter/src/pages/entry/ScoopOption.jsx
+++ sundae-starter/src/pages/entry/ScoopOption.jsx
@@ -1,5 +1,16 @@
-const ScoopOption = () => {
-    return <div>ScoopOption</div>;
+import Col from 'react-bootstrap';
+
+// eslint-disable-next-line react/prop-types
+const ScoopOption = ({ name, imagePath }) => {
+    return (
+        <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
+            <img
+                src={`http://localhost:3030/${imagePath}`}
+                alt={`${name} scoop`}
+                style={{ width: '75%' }}
+            />
+        </Col>
+    );
 };
 
 export default ScoopOption;
