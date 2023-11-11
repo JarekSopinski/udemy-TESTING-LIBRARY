4901501f38505ad83fa3a2bc33749dd4f448e1a1
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-11 18:08:55 +0100

    Replaced fireEvent with userEvent

--- sundae-starter/src/pages/tests/SummaryForm.test.jsx
+++ sundae-starter/src/pages/tests/SummaryForm.test.jsx
@@ -1,4 +1,5 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SummaryForm from '../summary/SummaryForm';
 
 test('Initial conditions', () => {
@@ -11,15 +12,17 @@ test('Initial conditions', () => {
     expect(confirmButton).toBeDisabled();
 })
 
-test('Checkbox enabled button on first click and disables on second click', () => {
+test('Checkbox enabled button on first click and disables on second click', async () => {
+    const user = userEvent.setup();
+
     render(<SummaryForm />);
 
     const checkbox = screen.getByRole('checkbox', { name: /terms and conditions/i });
     const confirmButton = screen.getByRole('button', { name: /confirm order/i });
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(confirmButton).toBeEnabled();
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(confirmButton).toBeDisabled();
 })
