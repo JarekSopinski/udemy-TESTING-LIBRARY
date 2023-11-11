bf5952f7cab9b4184bf4450c923ee6f2722e790e
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-11 19:10:10 +0100

    MODULE START - SIMULATING SERVER RESPONSE WITH MOCK SERVICE WORKER

---
+++ sundae-starter/src/mocks/handlers.js
@@ -0,0 +1,11 @@
+import { http } from 'msw'
+ 
+export const handlers = [
+  // Intercept the "GET /resource" request.
+  http.get('http://localhost:3030/scoops', () => {
+    return new Response([
+        {name: 'Chocolate', imagePath: '/images/chocolate.png'},
+        {name: 'Vanilla', imagePath: '/images/vanilla.png'},
+    ]);
+  })
+];
