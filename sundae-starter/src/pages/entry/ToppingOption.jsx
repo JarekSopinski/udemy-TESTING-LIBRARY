eebcc355b9e2e502549f245d54021062c1e4cabe
JarekSopinski <jarek.sopinski@gmail.com>
2024-05-06 05:45:24 +0200

    React code fot toppings checkboxes

--- sundae-starter/src/pages/entry/ToppingOption.jsx
+++ sundae-starter/src/pages/entry/ToppingOption.jsx
@@ -1,7 +1,15 @@
-import { Col } from "react-bootstrap";
+import { Col, Form } from "react-bootstrap";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 // eslint-disable-next-line react/prop-types
 const ToppingOption = ({ name, imagePath }) => {
+
+    const { updateItemCount } = useOrderDetails();
+
+    const handleChange = e => {
+        updateItemCount(name, e.target.checked ? 1 : 0, 'toppings');
+    };
+
     return (
         <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
             <img
@@ -9,6 +17,13 @@ const ToppingOption = ({ name, imagePath }) => {
                 alt={`${name} topping`}
                 style={{ width: '75%' }}
             />
+            <Form.Group controlId={`${name}-topping-checkbox`}>
+                <Form.Check
+                    type="checkbox"
+                    label={name}
+                    onChange={handleChange}
+                />
+            </Form.Group>
         </Col>
     );
 };
